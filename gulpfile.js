6c18277a02afbb3197ce014b4bac01ad3ec72814
RiaRiver <60794418+RiaRiver@users.noreply.github.com>
2020-03-03 02:41:06 +0300

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const minifyCSS = require("gulp-csso");
-const rename = require("gulp-rename");
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const minifyCSS = require('gulp-csso')
+const rename = require('gulp-rename')
 
 // Static server
-gulp.task("browser-sync", function () {
+gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: "./src/"
+      baseDir: './src/'
     }
-  });
-  gulp.watch("./src/*.html").on("change", browserSync.reload);
-});
+  })
+  gulp.watch('./src/*.html').on('change', browserSync.reload)
+})
 
-gulp.task("min-css", () => {
+gulp.task('min-css', () => {
   return gulp
-    .src("./src/css/*.css")
+    .src('./src/css/*.css')
     .pipe(minifyCSS())
     .pipe(
       rename({
-        suffix: ".min"
+        suffix: '.min'
       })
     )
-    .pipe(gulp.dest("./dist/css"));
-});
+    .pipe(gulp.dest('./dist/css'))
+})

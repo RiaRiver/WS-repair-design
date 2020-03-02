efe90d0fcea2112d56ffa3f568d6dbd703b73681
RiaRiver <60794418+RiaRiver@users.noreply.github.com>
2020-03-02 23:31:37 +0300

    create gulp-task to minimize css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const minifyCSS = require("gulp-csso");
+const rename = require("gulp-rename");
 
 // Static server
 gulp.task("browser-sync", function () {
@@ -9,4 +11,16 @@ gulp.task("browser-sync", function () {
     }
   });
   gulp.watch("./src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("min-css", () => {
+  return gulp
+    .src("./src/css/*.css")
+    .pipe(minifyCSS())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest("./dist/css"));
 });

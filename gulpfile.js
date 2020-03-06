097f52335e12b373c2748d9f47fb3375670a0a57
RiaRiver <60794418+RiaRiver@users.noreply.github.com>
2020-03-06 04:22:43 +0300

    refact: add scss to watch function in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,12 @@ function bs () {
   })
   watch('./src/*.html').on('change', browserSync.reload)
   watch('./src/sass/**/*.sass', serveSass)
+  watch('./src/sass/**/*.scss', serveSass)
   watch('./src/js/*.js').on('change', browserSync.reload)
 }
 
 function serveSass () {
-  return src('./src/sass/*.sass')
+  return src('./src/sass/**/*.sass')
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(dest('./src/css'))

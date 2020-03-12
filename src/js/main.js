cccd121df443f7a9b544771cfd854fc5a9bcc68e
RiaRiver <60794418+RiaRiver@users.noreply.github.com>
2020-03-12 05:27:37 +0300

    feat: add modal close on esc or space around

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,31 @@ document.addEventListener('DOMContentLoaded', function (event) {
   const modal = document.querySelector('.modal')
   const modalBtn = document.querySelectorAll('[data-toggle=modal]')
   const closeBtn = document.querySelector('.modal__close')
-  const swithModal = () => {
+  const switchModal = () => {
     modal.classList.toggle('modal--visible')
   }
 
+  function isModal () {
+    return modal.classList.contains('modal--visible')
+  }
+
   modalBtn.forEach(element => {
-    element.addEventListener('click', swithModal)
+    element.addEventListener('click', switchModal)
   })
 
-  closeBtn.addEventListener('click', swithModal)
+  closeBtn.addEventListener('click', switchModal)
+
+  document.addEventListener('keydown', (event) => {
+    var key = event.key || event.keyCode
+
+    if ((key === 'Escape' || key === 'Esc' || key === 27) && isModal()) {
+      switchModal()
+    }
+  })
+
+  document.addEventListener('click', (event) => {
+    if (isModal() && event.target.classList.contains('modal')) {
+      switchModal()
+    }
+  })
 })
